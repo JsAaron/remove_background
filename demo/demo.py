4edb6665c094a292f1ddb44c4d6f9b3698b92a21
aaron <401545553@qq.com>
2018-11-13 09:43:26 +0800

    1

--- demo/demo.py
+++ demo/demo.py
@@ -5,14 +5,9 @@ import util
 import colorsys
 import sys
 import os
-imageURl = getImagePath("8.jpg")
 
+imageURl = getImagePath("legao/6.jpg")
 
-def get_min_max(value,min,max):
-    r = value[0]
-    b = value[1]
-    g = value[2]
-    a = value[3]
 
 
 # 获取颜色的范围
@@ -22,57 +17,72 @@ def get_color_range(image):
     topLeft = image.getpixel((10,10)) 
     topRight = image.getpixel((w-10,10)) 
     bottomLeft = image.getpixel((10,h-10)) 
-    bottomRight = image.getpixel((w-10,h-10)) 
-    # (45, 46, 48, 255) (47, 48, 50, 255) (54, 56, 55, 255) (54, 56, 55, 255)
-    min=[45,46,48]
-    max=[54,56,55]
-    return (min,max)
+    bottomRight = image.getpixel((w-10,h-10))
+    maxR = max(topLeft[0],topRight[0],bottomLeft[0],bottomRight[0])
+    maxG = max(topLeft[1],topRight[1],bottomLeft[1],bottomRight[1])
+    maxB = max(topLeft[2],topRight[2],bottomLeft[2],bottomRight[2])
+    return ([0,0,0],[maxR+30,maxG+30,maxB+30])
 
 
 # 切图
 def cutFigure(url):
   # 指定宽高，位置中间
-  w = 1000 
   h = 700
-  name, ext = os.path.splitext(url)
+#   name, ext = os.path.splitext(url)
   im = Image.open(url)
   if im.mode != "RGBA":
       im = im.convert('RGBA')
   orgw,orgh = im.size
   # 中间位置
-  x = orgw/2-w/2
+  x = 0
   y = orgh/2-h/2
-  
-  region = im.crop((x, y, x+w, y+h))
+  region = im.crop((x, y, orgw, y+h))
   # region.save(name + '.png')
   # return Image.open(name + '.png')
   return region
 
 
 # 替换颜色
-def chromaKey(im,min,max):
-    min_hsv = util.rgb2hsv(min[0], min[1], min[2])
-    max_hsv = util.rgb2hsv(max[0], max[1], max[2])
-
-    pix = im.load()
+def chromaKey(im,minPixel,maxPixel):
+    min_r, min_g, min_b = minPixel
+    max_r, max_g, max_b = maxPixel
     width, height = im.size
     for x in range(width):
         for y in range(height):
-            r, g, b, a = pix[x, y]
-            h, s, v = util.rgb2hsv(r, g, b)
-            # h, s, v = (h_ratio * 360, s_ratio * 255, v_ratio * 255)
-
-            min_h, min_s, min_v = min_hsv
-            max_h, max_s, max_v = max_hsv
-
-            if min_h <= h <= max_h and min_s <= s <= max_s and min_v <= v <= max_v:
-                pix[x, y] = (0, 0, 0, 0)
-   
-     im.show()
+           r,g,b,alpha = im.getpixel((x,y))
+           if min_r <= r <= max_r and min_g <= g <= max_g and min_b <= b <= max_b:
+               im.putpixel((x,y),(0,0,0,0))
+               
+    return im
 
 
-# ChromaKey(imageURl)
+# 获取主要颜色
+def get_dominant_color(image):
+    count,r,g,b = image.getcolors()
+    print(count)
+    # max_score = 0.0001
+    # dominant_color = None
+    # for count,(r,g,b) in image.getcolors(image.size[0]*image.size[1]):
+    #     # 转为HSV标准
+    #     saturation = colorsys.rgb_to_hsv(r/255.0, g/255.0, b/255.0)[1]
+    #     y = min(abs(r*2104+g*4130+b*802+4096+131072)>>13,235)
+    #     y = (y-16.0)/(235-16)
+    #     #忽略高亮色
+    #     if y > 0.9:
+    #         continue
+    #     score = (saturation+0.1)*count
+    #     if score > max_score:
+    #         max_score = score
+    #         dominant_color = (r,g,b)
+    # return dominant_color
+
 if __name__ == '__main__':
     image = cutFigure(imageURl)
-    min,max=get_color_range(image)
-    chromaKey(image,min,max)
+    minPixel,maxPixel=get_color_range(image)
+    chromaImage = chromaKey(image,minPixel,maxPixel)
+    name, ext = os.path.splitext(imageURl)
+    # chromaImage.save(name + '.png')
+    get_dominant_color(chromaImage)
+
+    # newIm = Image.new("RGB", (640, 480), mainColor)
+    # chromaImage.show()
